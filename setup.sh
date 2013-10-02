cae13270081edf23ff7202fdef6431168bf620f7
medikid <soma.programmer@gmail.com>
2013-10-01 23:13:50 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,39 +1,27 @@
 #!/bin/sh -e
 
-#create a TheGoldminerPi directory
-sudo mkdir TheGoldminerPi
-
-#cd into folder
-cd TheGoldminerPi
+#come out of TheGoldminerPi folder to create a TheGoldminerPi directory
+cd ..
 
 #git clone bfgminer file
-sudo apt-get update
-apt-get install git autoconf automake libtool pkg-config libcurl4-gnutls-dev libjansson-dev uthash-dev libncurses5-dev
+sudo apt-get -m -f update
+apt-get -m -f install autoconf automake libtool pkg-config libcurl4-gnutls-dev libjansson-dev uthash-dev libncurses5-dev
 git clone git://github.com/luke-jr/bfgminer.git
 
 #install bfgminer
+cd bfgminer
 ./autogen.sh
 ./configure --disable-opencl
 make
-
+cd ..
 
 #setup autostart bfgminer
-autostartCmd = '
-cd ~/Desktop/TheGoldminerPi
-nohup ./bfgminer/bfgminer --config TheGoldminer.conf -S all  >/dev/null 2>&1&
+sudo sed -i '/exit 0/i \
+ cd ~/Desktop/Bitcoin \
+ nohup ./bfgminer/bfgminer --config TheGoldminerPi/TheGoldminer.conf -S all  >/dev/null 2>&1& \
 
-'
+' /etc/rc.local;
 
-lnum=($(sed -n '/<\/head>/=' /etc/rc.local ))  # make array of line numbers
-((lnum>0)) && sed -i "${lnum[$((${#lnum[@]}-1))]}i \
-                      $autostartCmd" /etc/rc.local
-
-
-
-#git clone TheGoldminerPi
-git clone git://github.com/medikid/TheGoldminerPi.git
-
-#copy TheGoldminer.conf
 
 #install apache2 and php
 sudo apt-get install apache2
@@ -44,7 +32,12 @@ sudo /etc/init.d/apache2 reload
 
 #create simlink of miner.php, api-example.php in var/www
 cd /var/www/
-ln -s /home/pi/Desktop/Bitcoin/TheGoldminer/bfgminer/miner.php ./miner.php
-ln -s /home/pi/Desktop/Bitcoin/TheGoldminer/bfgminer/api-example.php ./api-example.php
+sudo mkdir -p TheGoldminer
+cd TheGoldminer
+ln -s /home/pi/Desktop/Bitcoin/bfgminer/miner.php ./miner.php
+ln -s /home/pi/Desktop/Bitcoin/bfgminer/api-example.php ./api-example.php
+
+
+
 
 

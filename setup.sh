eea66c23b061d22cfa1c54ed02600b8c1a80193d
medikid <soma.programmer@gmail.com>
2013-10-01 23:54:54 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,10 @@
 #come out of TheGoldminerPi folder to create a TheGoldminerPi directory
 cd ..
 
-#git clone bfgminer file
+#update dependenaceis and git clone bfgminer file
+sudo apt-get -m -f upgrade
 sudo apt-get -m -f update
-apt-get -m -f install autoconf automake libtool pkg-config libcurl4-gnutls-dev libjansson-dev uthash-dev libncurses5-dev
+sudo apt-get -m -f install autoconf automake libtool pkg-config libcurl4-gnutls-dev libjansson-dev uthash-dev libncurses5-dev
 git clone git://github.com/luke-jr/bfgminer.git
 
 #install bfgminer

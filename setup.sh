ab23ff5f35953cba93976410f1221a216a8873fe
medikid <soma.programmer@gmail.com>
2013-10-12 18:13:34 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,8 @@ cd ..
 #update dependenaceis and git clone bfgminer file
 sudo apt-get -m -f upgrade
 sudo apt-get -m -f update
-sudo apt-get -m -f install autoconf automake libtool pkg-config libcurl4-gnutls-dev libjansson-dev uthash-dev libncurses5-dev
+sudo apt-get -m -f install build-essential autoconf automake libtool pkg-config libcurl4-gnutls-dev libjansson-dev uthash-dev libncurses5-dev libudev-dev libusb-1.0-0-dev libmicrohttpd-dev curl
+
 git clone git://github.com/luke-jr/bfgminer.git
 
 #install bfgminer

c04109d572c94f1e8bcbacc0027c0e3a34025788
medikid <soma.programmer@gmail.com>
2013-10-15 00:05:19 -0700

    Update installBFGMiner.sh

--- installBFGMiner.sh
+++ installBFGMiner.sh
@@ -1,13 +1,9 @@
 #!/bin/sh -e
 
 #come out of TheGoldminerPi folder to create a TheGoldminerPi directory
-cd ..
+cd ~/Desktop/Bitcoin/
 
-#update dependenaceis and git clone bfgminer file
-sudo apt-get -m -f upgrade
-sudo apt-get -m -f update
-sudo apt-get -m -f install build-essential autoconf automake libtool pkg-config libcurl4-gnutls-dev libjansson-dev uthash-dev libncurses5-dev libudev-dev libusb-1.0-0-dev libmicrohttpd-dev curl
-
+#clone git repository of bfgminer
 git clone git://github.com/luke-jr/bfgminer.git
 
 #install bfgminer
@@ -17,30 +13,3 @@ cd bfgminer
 
 make
 cd ..
-
-#setup autostart bfgminer
-sudo sed -i '/exit 0/i \
- cd ~/Desktop/Bitcoin \
- nohup ./bfgminer/bfgminer --config TheGoldminerPi/miner_config.conf -S all  >/dev/null 2>&1& \
-
-' /etc/rc.local;
-
-
-#install apache2 and php
-sudo apt-get install apache2
-sudo apt-get install php5
-sudo /etc/init.d/apache2 reload
-
-
-
-#create simlink of miner.php, api-example.php in var/www
-cd /var/www/
-sudo mkdir -p TheGoldminer
-cd TheGoldminer
-ln -s /home/pi/Desktop/Bitcoin/bfgminer/miner.php ./miner.php
-ln -s /home/pi/Desktop/Bitcoin/bfgminer/api-example.php ./api-example.php
-
-
-
-
-

240119cd4221ca2cd3118605ba5f6cb643e37817
medikid <soma.programmer@gmail.com>
2013-10-15 00:14:47 -0700

    Update installBFGMiner.sh

--- installBFGMiner.sh
+++ installBFGMiner.sh
@@ -4,12 +4,12 @@
 cd ~/Desktop/Bitcoin/
 
 #clone git repository of bfgminer
-git clone git://github.com/luke-jr/bfgminer.git
+git clone git://github.com/luke-jr/bfgminer.git ;
 
 #install bfgminer
 cd bfgminer
-./autogen.sh
-./configure --disable-avalon --disable-opencl --disable-adl  --disable-bitfury  --disable-bigpic  --disable-littlefury --disable-bitforce   --disable-modminer --disable-x6500 --disable-ztex --with-system-libblkmaker
+sudo ./autogen.sh ;
+sudo ./configure --disable-avalon --disable-opencl --disable-adl  --disable-bitfury  --disable-bigpic  --disable-littlefury --disable-bitforce   --disable-modminer --disable-x6500 --disable-ztex --with-system-libblkmaker ;
 
-make
+sudo make;
 cd ..

8754d08514bafade8e7963b4f670f6a9cb34b183
medikid <soma.programmer@gmail.com>
2013-10-15 00:50:14 -0700

    Update setupBRIDGE.sh

--- setupBRIDGE.sh
+++ setupBRIDGE.sh
@@ -1,46 +1,116 @@
-#!/bin/sh -e
+#!/bin/bash
+#
+# Site: www.hackhappy.org
+# Article: http://hackhappy.org/uncategorized/how-to-use-a-raspberry-pi-to-create-a-wireless-to-wired-network-bridge/
+# Video: http://youtu.be/FlLLmacDqJU
+# Description: Shells script to configure linux to forward traffic
+# from wireless to ethernet. This is useful if you do not have wire 
+# access to the router.
 
-#come out of TheGoldminerPi folder to create a TheGoldminerPi directory
-cd ..
+function ConfigNetBridge () {
+	clear
+	echo "############################################################"
+	echo "# Configure linux to connect wifi network to wired network #"
+	echo "############################################################"
+	echo ""
+	echo -n "Input LAN IP [192.168.0.1]: "
+	read lip ;
+	if [ "$lip" = "" ]; then
+			lip="192.168.0.1"
+	fi
 
-#update dependenaceis and git clone bfgminer file
-sudo apt-get -m -f upgrade
-sudo apt-get -m -f update
-sudo apt-get -m -f install build-essential autoconf automake libtool pkg-config libcurl4-gnutls-dev libjansson-dev uthash-dev libncurses5-dev libudev-dev libusb-1.0-0-dev libmicrohttpd-dev curl
+	echo -n "Input LAN Netmask [255.255.255.0]: "
+	read netmask ;
+	if [ "$netmask" = "" ]; then
+			netmask="255.255.255.0"
+	fi
 
-git clone git://github.com/luke-jr/bfgminer.git
+	echo -n "Input LAN Subnet [192.168.0.0]: "
+	read subnet ;
+	if [ "$subnet" = "" ]; then
+			subnet="192.168.0.0"
+	fi
 
-#install bfgminer
-cd bfgminer
-./autogen.sh
-./configure --disable-avalon --disable-opencl --disable-adl  --disable-bitfury  --disable-bigpic  --disable-littlefury --disable-bitforce   --disable-modminer --disable-x6500 --disable-ztex --with-system-libblkmaker
+	echo -n "Input IP Range Start [192.168.0.2]: "
+	read ipstart ;
+	if [ "$ipstart" = "" ]; then
+			ipstart="192.168.0.2"
+	fi
 
-make
-cd ..
+	echo -n "Input IP Range End [192.168.0.200]: "
+	read ipend ;
+	if [ "$ipend" = "" ]; then
+			ipend="192.168.0.200"
+	fi
 
-#setup autostart bfgminer
-sudo sed -i '/exit 0/i \
- cd ~/Desktop/Bitcoin \
- nohup ./bfgminer/bfgminer --config TheGoldminerPi/miner_config.conf -S all  >/dev/null 2>&1& \
+	echo -n "Input Wifi SSID: "
+	read ssid ;
+	echo -n "Input Wifi Password: "
+	read password ;
 
-' /etc/rc.local;
+	echo -n "Input LAN Device [eth0]: "
+	read landv ;
+	if [ "$land" = "" ]; then
+			land="eth0"
+	fi
 
+	echo -n "Input Wifi Device [wlan0]: "
+	read wifid ;
+	if [ "$wifid" = "" ]; then
+			wifid="wlan0"
+	fi
 
-#install apache2 and php
-sudo apt-get install apache2
-sudo apt-get install php5
-sudo /etc/init.d/apache2 reload
+	#Update system software
+	clear
 
+	#Configure devices
+	echo "
+			auto lo $land
+					iface lo inet loopback
+			iface $land inet static
+							address $lip
+							netmask $netmask
 
+			auto $wifid
+			iface $wifid inet dhcp
+			wpa-ssid \"$ssid\"
+			wpa-psk \"$password\"
 
-#create simlink of miner.php, api-example.php in var/www
-cd /var/www/
-sudo mkdir -p TheGoldminer
-cd TheGoldminer
-ln -s /home/pi/Desktop/Bitcoin/bfgminer/miner.php ./miner.php
-ln -s /home/pi/Desktop/Bitcoin/bfgminer/api-example.php ./api-example.php
+			up iptables-restore > /etc/iptables.ipv4.nat
+	" > /etc/network/interfaces
 
+	#install and configure the DHCP server
+	clear
 
+	echo "
+			option domain-name \"wifi2lan.rpi\";
+			option domain-name-servers 8.8.8.8, 8.8.4.4;
+			subnet $subnet netmask $netmask {
+					range $ipstart $ipend;
+					option routers $lip;
+			}
+	" > /etc/dhcp/dhcpd.conf
+	clear
+	echo "##### Restarting DHCP Server..."
+	echo ""
+	echo "INTERFACES=\"$land\"" >  /etc/default/isc-dhcp-server
+	service isc-dhcp-server restart
+	update-rc.d isc-dhcp-server enable
 
+	#Install and configure iptables
+	clear
 
+	 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+	 echo "1" > /proc/sys/net/ipv4/ip_forward
+	 iptables -t nat -A POSTROUTING -o $wifid -j MASQUERADE
+	 iptables -A FORWARD -i $wifid -o $land -m state --state RELATED,ESTABLISHED -j ACCEPT
+	 iptables -A FORWARD -i $land -o $wifid -j ACCEPT
+	 iptables-save > /etc/iptables.ipv4.nat
 
+	clear
+	echo "##### Installation Complete."
+	echo "##### Reboot or restart networking to apply new network configuration: sudo /etc/init.d/networking"
+
+ }
+
+sudo ConfigNetBridge ;

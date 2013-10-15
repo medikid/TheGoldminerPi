56612b244bb45f671c7352b75901cd1fb2151f46
medikid <soma.programmer@gmail.com>
2013-10-15 00:53:46 -0700

    Update installDeps.sh

--- installDeps.sh
+++ installDeps.sh
@@ -1,3 +1,3 @@
 #! /bin/sh -e
 
-sudo apt-get -m -f install build-essential autoconf automake libtool pkg-config libcurl4-gnutls-dev libjansson-dev uthash-dev libncurses5-dev libudev-dev libusb-1.0-0-dev libmicrohttpd-dev curl isc-dhcp-server iptables;
+sudo apt-get -m -f install build-essential autoconf automake libtool pkg-config libcurl4-gnutls-dev libjansson-dev uthash-dev libncurses5-dev libudev-dev libusb-1.0-0-dev libmicrohttpd-dev curl apache2 php5 isc-dhcp-server iptables;

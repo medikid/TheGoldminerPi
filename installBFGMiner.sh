a93219c635e4587dbe6305cfaa94b8947663fe9a
medikid <soma.programmer@gmail.com>
2013-10-16 10:36:12 -0700

    Update installBFGMiner.sh

--- installBFGMiner.sh
+++ installBFGMiner.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 #come out of TheGoldminerPi folder to create a TheGoldminerPi directory
-cd /home/pi/Desktop/Bitcoin/
+cd /home/pi/Desktop/Bitcoin ;
 
 #clone git repository of bfgminer
 git clone git://github.com/luke-jr/bfgminer.git ;
@@ -9,7 +9,7 @@ git clone git://github.com/luke-jr/bfgminer.git ;
 #install bfgminer
 cd bfgminer
 sudo ./autogen.sh ;
-sudo ./configure --disable-avalon --disable-opencl --disable-adl  --disable-bitfury  --disable-bigpic  --disable-littlefury --disable-bitforce   --disable-modminer --disable-x6500 --disable-ztex --with-system-libblkmaker ;
+sudo ./configure --disable-avalon --disable-opencl --disable-adl  --disable-bitfury  --disable-bigpic  --disable-littlefury --disable-bitforce   --disable-modminer --disable-x6500 --disable-ztex  ;
 
-sudo make;
-cd ..
+sudo make ;
+cd .. ;

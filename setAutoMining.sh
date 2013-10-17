a07164164a0552a3e088717cccc2310cbf379bc8
medikid <soma.programmer@gmail.com>
2013-10-17 05:06:17 -0400

    Update setAutoMining.sh

--- setAutoMining.sh
+++ setAutoMining.sh
@@ -4,8 +4,16 @@
 #setup autostart bfgminer
 sudo sed -i '/exit 0/i \
 cd /home/pi/Desktop/Bitcoin \
-
+\
+#bfgminer commands \
+#nohup ./bfgminer/bfgminer -o stratum.btcguild.com:3333 -u TheGoldminer_BLADE1 -p BLADE1 >/dev/null 2>&1& \
+#nohup ./bfgminer/bfgminer --http-port 8330 -o stratum.btcguild.com:3333 -u TheGoldminer_BLADE1 -p BLADE1 >/dev/null 2>&1& \
 nohup ./bfgminer/bfgminer --http-port 8330 -o stratum.btcguild.com:3333 -u TheGoldminer_BLADE1 -p BLADE1 >/dev/null 2>&1& \
-
-
-' etc/rc.local ;
+\
+\
+#stratum proxy commands \
+#nohup ./stratum-mining-proxy/mining_proxy.py >/dev/null 2>&1& \
+#nohup ./stratum-mining-proxy/mining_proxy.py -o stratum.btcguild.com -p 3333 >/dev/null 2>&1& \
+\
+\
+' /etc/rc.local ;

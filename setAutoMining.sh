890c8337767f8373e32054f98aae141ad55baf18
medikid <soma.programmer@gmail.com>
2013-10-17 06:20:26 -0400

    Update setAutoMining.sh

--- setAutoMining.sh
+++ setAutoMining.sh
@@ -1,22 +1,19 @@
 #!/bin/sh -e
 
-#there are two entries of exit 0, lets morph the first entry "exit 0"
-sudo sed -i '/"exit 0"/i \
-"--e x i t 0--" ' /etc/rc.local ;
-
 #setup autostart bfgminer
-sudo sed -i '/exit 0/i \
-cd /home/pi/Desktop/Bitcoin \
+sudo sed -i 's/fi/i \
+fi \
+\
 \
 #bfgminer commands \
-#nohup ./bfgminer/bfgminer -o stratum.btcguild.com:3333 -u TheGoldminer_BLADE1 -p BLADE1 >/dev/null 2>&1& \
-#nohup ./bfgminer/bfgminer --http-port 8330 -o stratum.btcguild.com:3333 -u TheGoldminer_BLADE1 -p BLADE1 >/dev/null 2>&1& \
-nohup ./bfgminer/bfgminer --http-port 8330 -o stratum.btcguild.com:3333 -u TheGoldminer_BLADE1 -p BLADE1 >/dev/null 2>&1& \
+#nohup ./home/pi/Desktop/Bitcoin/bfgminer/bfgminer -o stratum.btcguild.com:3333 -u TheGoldminer_BLADE1 -p BLADE1 >/dev/null 2>&1& \
+#nohup ./home/pi/Desktop/Bitcoin/bfgminer/bfgminer --http-port 8330 -o stratum.btcguild.com:3333 -u TheGoldminer_BLADE1 -p BLADE1 >/dev/null 2>&1& \
+nohup ./home/pi/Desktop/Bitcoin/bfgminer/bfgminer --http-port 8330 -o stratum.btcguild.com:3333 -u TheGoldminer_BLADE1 -p BLADE1 >/dev/null 2>&1& \
 \
 \
 #stratum proxy commands \
-#nohup ./stratum-mining-proxy/mining_proxy.py >/dev/null 2>&1& \
-#nohup ./stratum-mining-proxy/mining_proxy.py -o stratum.btcguild.com -p 3333 >/dev/null 2>&1& \
+#nohup ./home/pi/Desktop/Bitcoin/stratum-mining-proxy/mining_proxy.py >/dev/null 2>&1& \
+#nohup ./home/pi/Desktop/Bitcoin/stratum-mining-proxy/mining_proxy.py -o stratum.btcguild.com -p 3333 >/dev/null 2>&1& \
 \
 \
 ' /etc/rc.local ;

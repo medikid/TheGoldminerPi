ff924eb81f831b9945a9b38d121ec4f60eb91981
medikid <soma.programmer@gmail.com>
2013-10-17 17:21:31 -0700

    Update installStratumProxy.sh

--- installStratumProxy.sh
+++ installStratumProxy.sh
@@ -18,8 +18,8 @@ sudo python distribute_setup.py ;
 # This will install required dependencies (namely Twisted and Stratum libraries), but don't install the package into the system.
 sudo python setup.py develop ;
 
-
+#--------- MIDSTATE NOT POSSIBLE WTH RASPI VERSION - targe chip incomptaptible -------------#
 #compile with midstate c extensions, will speeds up midstate calculations
-cd midstatec ;
-sudo make ;
+#cd midstatec ;
+#sudo make ;
 cd .. ;
